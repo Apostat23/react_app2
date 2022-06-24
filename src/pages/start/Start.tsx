6c387d133f559467e7b6aa4ab479b103c9c49ab3
Diego Mota <diego.mota@wizeline.com>
2022-06-24 15:54:24 -0500

    Third commit

--- src/pages/start/Start.tsx
+++ src/pages/start/Start.tsx
@@ -1,14 +1,30 @@
 import React from 'react'
 
+type Person = { name: string, last: string, age: number }
+const person: Person = { name: 'Diego', last: 'Mota', age: 40}
+interface User { name: string, last: string, age: number }
+const user: User = { name: 'Diego', last: 'Mota', age: 40 }
+
+type Fish = { swim: () => "I swim" }
+const fish: Fish = { swim: () => "I swim" }
+type Bird = { fly: () => "I fly" }
+const bird: Bird = { fly: () => "I fly" }
+type Human = { swim: () => "I swim", fly: () => "I fly" }
+const human: Human = { swim: () => "I swim", fly: () => "I fly" }
+
+interface Circle { kind: 'circle', radius: number }
+interface Square { kind: 'square', sideLength: number }
+interface Rectangle { kind: 'rectangle', height: number, width: number }
+type Shape = Circle | Square | Rectangle
+const circle = { kind: 'circle', radius: 10 } as const
+const square = { kind: 'square', sideLength: 10 } as const
+const rectangle = { kind: 'rectangle', height: 10, width: 10 } as const
+
 export const Start = () => {
   const message: string = 'Hello World'
   const number: number = 0
   const names = ['Diego', 'Rafael', 'Gabriel']
   //const hash = { key1: 'value1', key2: 'value2' }
-  type Person = { name: string, last: string, age: number }
-  const person: Person = { name: 'Diego', last: 'Mota', age: 40}
-  interface User { name: string, last: string, age: number }
-  const user: User = { name: 'Diego', last: 'Mota', age: 40 }
   
   const string_1: string = 'Hello World'
   const string_2 = 'Hello World' as string
@@ -37,12 +53,12 @@ export const Start = () => {
     return `The number is ${number}`;
   }
 
-  const getSample = (array: string[]) => {
+  const getSampleFromArray = (array: string[]) => {
     return array[Math.floor(Math.random() * array.length)];
   }
 
   const displayRandomName = (names: string[]) => {
-    return getSample(names)
+    return getSampleFromArray(names)
   }
 
   const printCoordinates = (coordinates: {x: number, y: number}) => {
@@ -80,6 +96,48 @@ export const Start = () => {
     return 'There are no users online'
   }
 
+  const true_boolean = !!'Hello World'
+  const false_boolean = !'Hello World'
+
+  const move = (animal: Fish | Bird | Human) => {
+    if ('swim' in animal && 'fly' in animal) {
+      return 'I swim and fly'
+    } else if ('swim' in animal) {
+      return 'I swim'
+    } else 
+    if ('fly' in animal) {
+      return 'I fly'
+    }
+  }
+  
+  const isFish = (pet: Fish | Bird | Human): pet is Fish => {
+    return (pet as Fish).swim !== undefined
+  }
+
+  const testInstanceOf = (value: Date | string) => {
+    if (value instanceof Date) {
+      return 'It is a date'
+    } else {
+      return 'It is a string'
+    }
+  }
+  
+  const numString = Math.random() > 0.5 ? 0 : 'Hello World'
+  const numString2 = Math.random() > 0.5 ? 0 : 'Hello World'
+  const displayType = (value: number | string): number | string  => {
+    return `The value is ${typeof value}`
+  }
+
+  const getArea = (shape: Shape) => {
+    if (shape.kind === 'circle') {
+      return Math.PI * Math.pow(shape.radius!, 2)
+    } else if (shape.kind === 'square') {
+      return Math.pow(shape.sideLength!, 2)
+    } else if (shape.kind === 'rectangle') {
+      return shape.height * shape.width
+    }
+  }
+
   return (
     <div>
       <h1>Start</h1>
@@ -107,6 +165,19 @@ export const Start = () => {
       <p>{getUsersOnLineMessage(0)}</p>
       <p>{getUsersOnLineMessage(1)}</p>
       <p>{getUsersOnLineMessage(3)}</p>
+      <p>boolean {true_boolean ? true.toString() : false.toString()}</p>
+      <p>boolean {false_boolean ? true.toString() : false.toString()}</p>
+      <p>{move(fish)}</p>
+      <p>{move(bird)}</p>
+      <p>{move(human)}</p>
+      <p>{testInstanceOf(new Date()) }</p>
+      <p>{testInstanceOf('Hello') }</p>
+      <p>{displayType(numString)}</p>
+      <p>{displayType(numString2)}</p>
+      <p>{isFish(fish) ? fish.swim() : 'I am not a fish'}</p>
+      <p>circle {getArea(circle)}</p>
+      <p>square {getArea(square)}</p>
+      <p>rectangle {getArea(rectangle)}</p>
     </div>
   )
 }
